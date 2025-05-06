88023eab87cb087fc24009ac88e45f0bae507158
gabryel <gabryelaraujo71@gmail.com>
2025-05-06 16:07:18 -0300

    feat: pagina home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,80 @@
-import { MainContainer } from "../../components/MainContainer";
+import {
+  ArrowDown,
+  ArrowUp,
+  Eye,
+  EyeOff,
+  PiggyBank,
+  WalletCards,
+} from "lucide-react";
+import { Container } from "../../components/Container";
+import Nav from "../../components/Nav";
 import styles from "./style.module.css";
-
-import { Eye } from "lucide-react";
+import { useContext } from "react";
+import { BalanceCard } from "../../components/BalanceCard";
+import { ViewContext } from "../../contexts/ViewContext";
 
 export function Home() {
+  const { view, setView } = useContext(ViewContext);
+
+  type cardTypes = {
+    title: string;
+    type: "balance" | "receipt" | "expense" | "card";
+    color: "blue" | "green" | "red" | "olive";
+    icon: React.ReactNode;
+  };
+
+  const cards: cardTypes[] = [
+    {
+      title: "Saldo",
+      color: "blue",
+      type: "balance",
+      icon: <PiggyBank color="white" />,
+    },
+    {
+      title: "Receitas",
+      color: "green",
+      type: "receipt",
+      icon: <ArrowUp color="white" />,
+    },
+    {
+      title: "Despesas",
+      color: "red",
+      type: "expense",
+      icon: <ArrowDown color="white" />,
+    },
+    {
+      title: "Cartão de Crédito",
+      color: "olive",
+      type: "card",
+      icon: <WalletCards color="white" />,
+    },
+  ];
+
   return (
-    <MainContainer>
-      <p>Saldo em conta</p>
-      <div className={styles.inline}>
-        <p>12.476,90</p>
-        <Eye />
-      </div>
-    </MainContainer>
+    <>
+      <Nav />
+      <Container>
+        <div className={styles.inline}>
+          <h1>Dashboard</h1>
+          {view ? (
+            <Eye onClick={() => setView(!view)} />
+          ) : (
+            <EyeOff onClick={() => setView(!view)} />
+          )}
+        </div>
+        <div className={styles.gridContainer}>
+          {cards.map((card) => {
+            return (
+              <BalanceCard
+                title={card.title}
+                color={card.color}
+                type={card.type}
+                icon={card.icon}
+              ></BalanceCard>
+            );
+          })}
+        </div>
+      </Container>
+    </>
   );
 }
