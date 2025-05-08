e5cad3d6b886d28f45b2eaf12cc79af65993fa92
gabryel <gabryelaraujo71@gmail.com>
2025-05-07 22:16:36 -0300

    feat: adicionada a nova lógica da tela inicial

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,79 +1,21 @@
-import {
-  ArrowDown,
-  ArrowUp,
-  Eye,
-  EyeOff,
-  PiggyBank,
-  WalletCards,
-} from "lucide-react";
 import { Container } from "../../components/Container";
 import Nav from "../../components/Nav";
-import styles from "./style.module.css";
+import { MainCards } from "../../components/MainCards";
 import { useContext } from "react";
-import { BalanceCard } from "../../components/BalanceCard";
 import { ViewContext } from "../../contexts/ViewContext";
+import { EditTransaction } from "../../components/EditTransaction";
 
 export function Home() {
-  const { view, setView } = useContext(ViewContext);
-
-  type cardTypes = {
-    title: string;
-    type: "balance" | "receipt" | "expense" | "card";
-    color: "blue" | "green" | "red" | "olive";
-    icon: React.ReactNode;
-  };
-
-  const cards: cardTypes[] = [
-    {
-      title: "Saldo",
-      color: "blue",
-      type: "balance",
-      icon: <PiggyBank color="white" />,
-    },
-    {
-      title: "Receitas",
-      color: "green",
-      type: "receipt",
-      icon: <ArrowUp color="white" />,
-    },
-    {
-      title: "Despesas",
-      color: "red",
-      type: "expense",
-      icon: <ArrowDown color="white" />,
-    },
-    {
-      title: "Cartão de Crédito",
-      color: "olive",
-      type: "card",
-      icon: <WalletCards color="white" />,
-    },
-  ];
+  const { screen } = useContext(ViewContext);
 
   return (
     <>
       <Nav />
       <Container>
-        <div className={styles.inline}>
-          <h1>Dashboard</h1>
-          {view ? (
-            <Eye onClick={() => setView(!view)} />
-          ) : (
-            <EyeOff onClick={() => setView(!view)} />
-          )}
-        </div>
-        <div className={styles.gridContainer}>
-          {cards.map((card) => {
-            return (
-              <BalanceCard
-                title={card.title}
-                color={card.color}
-                type={card.type}
-                icon={card.icon}
-              ></BalanceCard>
-            );
-          })}
-        </div>
+        {screen === "main" && <MainCards />}
+        {(screen === "receipt" ||
+          screen === "expense" ||
+          screen === "balance") && <EditTransaction type={screen} />}
       </Container>
     </>
   );
