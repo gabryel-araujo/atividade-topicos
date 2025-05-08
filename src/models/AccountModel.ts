ac2925a9f39d6b5f14a8136d8127f26d1719cf54
gabryel <gabryelaraujo71@gmail.com>
2025-05-07 22:13:31 -0300

    feat: ajuste no modelo da aplicacao

--- src/models/AccountModel.ts
+++ src/models/AccountModel.ts
@@ -1,3 +1,5 @@
+import type { BillType } from "../types/types";
+
 export type AccountModel = {
   id: string;
   name: string;
@@ -5,12 +7,18 @@ export type AccountModel = {
   receipts: number;
   expenses: number;
   card: number;
-  view: boolean;
+  arrReceipts: BillType[];
+  arrExpenses: BillType[];
+  arrCard: BillType[];
 };
+
+export type Screens = "main" | "receipt" | "expense" | "balance";
 
 export type AccountContextType = {
   accountData: AccountModel;
   setAccountData: React.Dispatch<React.SetStateAction<AccountModel>>;
   view: boolean;
   setView: React.Dispatch<React.SetStateAction<boolean>>;
+  screen: string;
+  setScreen: React.Dispatch<React.SetStateAction<Screens>>;
 };
