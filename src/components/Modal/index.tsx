7f3ddb5f423cace920d87970633d2cf43670481a
gabryel <gabryelaraujo71@gmail.com>
2025-05-08 12:49:56 -0300

    feat: implementacao do zod no modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -2,62 +2,78 @@ import * as Dialog from "@radix-ui/react-dialog";
 import { X } from "lucide-react";
 import "./styles.css";
 import type React from "react";
-import { TransactionDict, type BillType } from "../../types/types";
+import {
+  categoriesData,
+  TransactionDict,
+  type BillType,
+  type Categories,
+} from "../../types/types";
 import { MainContainer } from "../MainContainer";
-import { useContext, useRef } from "react";
+import { useContext } from "react";
 import { ViewContext } from "../../contexts/ViewContext";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { toast } from "sonner";
+
+const schema = z.object({
+  type: z.string().min(3, "O tipo precisa de pelo menos 3 caracteres"),
+  value: z.coerce.number().positive("O valor inserido deve ser maior que 0"),
+  category: z.string().min(1, "Selecione uma opção"),
+});
+
+type FormData = z.infer<typeof schema>;
 
 type ModalProps = {
   open: boolean;
   setOpen: React.Dispatch<React.SetStateAction<boolean>>;
-  type: "receipt" | "expense" | "balance";
+  type: "receipt" | "expense" | "balance" | "card";
 };
 
 export function Modal({ open, setOpen, type }: ModalProps) {
   const { setAccountData } = useContext(ViewContext);
-  const titleRef = useRef<HTMLInputElement>(null);
-  const valueRef = useRef<HTMLInputElement>(null);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
 
-  function handleVerifyTransaction() {
+  function handleVerifyTransaction(data: FormData) {
+    const newTransaction: BillType = {
+      title: data.type,
+      value: data.value,
+      category: data.category as Categories,
+    };
+
     if (type === "receipt") {
-      handleAddReceipt();
+      setAccountData((prev) => ({
+        ...prev,
+        arrReceipts: [...prev.arrReceipts, newTransaction],
+      }));
     } else if (type === "expense") {
-      handleAddExpense();
+      setAccountData((prev) => ({
+        ...prev,
+        arrExpenses: [...prev.arrExpenses, newTransaction],
+      }));
+    } else if (type === "card") {
+      setAccountData((prev) => ({
+        ...prev,
+        arrCard: [...prev.arrCard, newTransaction],
+      }));
     }
+
+    setOpen(false);
+    toast.success(`${TransactionDict[type].title} cadastrada com sucesso!`);
   }
 
-  function handleAddReceipt() {
-    const newReceipt: BillType = {
-      title: titleRef.current!.value,
-      value: Number(valueRef.current!.value),
-    };
-
-    setAccountData((prev) => ({
-      ...prev,
-      arrReceipts: [...prev.arrReceipts, newReceipt],
-    }));
-  }
-
-  function handleAddExpense() {
-    const newExpense: BillType = {
-      title: titleRef.current!.value,
-      value: Number(valueRef.current!.value),
-    };
-
-    setAccountData((prev) => ({
-      ...prev,
-      arrExpenses: [...prev.arrExpenses, newExpense],
-    }));
-  }
+  const label = type !== "card" ? `${TransactionDict[type].title}s` : "Despesa";
 
   return (
     <Dialog.Root onOpenChange={setOpen} open={open}>
       <Dialog.Portal>
         <Dialog.Overlay className="DialogOverlay" />
         <Dialog.Content className="DialogContent">
-          <Dialog.Title className="DialogTitle">
-            Adicionar {TransactionDict[type].title}
-          </Dialog.Title>
+          <Dialog.Title className="DialogTitle">Adicionar {label}</Dialog.Title>
           <MainContainer>
             <div className="Fildset">
               <label className="Label" htmlFor="type">
@@ -66,9 +82,25 @@ export function Modal({ open, setOpen, type }: ModalProps) {
               <input
                 className="Input"
                 id="type"
-                placeholder="Premiação"
-                ref={titleRef}
+                placeholder="Digite aqui..."
+                {...register("type")}
               />
+              {errors.type && <p className="error">{errors.type.message}</p>}
+            </div>
+
+            <div className="Fildset">
+              <label className="Label" htmlFor="category">
+                Categoria
+              </label>
+              <select className="Input" id="category" {...register("category")}>
+                <option value="">Selecione...</option>
+                {categoriesData.map((category) => {
+                  return <option value={category}>{category}</option>;
+                })}
+              </select>
+              {errors.category && (
+                <p className="error">{errors.category.message}</p>
+              )}
             </div>
 
             <div className="Fildset">
@@ -79,9 +111,10 @@ export function Modal({ open, setOpen, type }: ModalProps) {
                 className="Input"
                 id="value"
                 placeholder="R$2000,00"
-                ref={valueRef}
                 type="number"
+                {...register("value")}
               />
+              {errors.value && <p className="error">{errors.value.message}</p>}
             </div>
           </MainContainer>
 
@@ -95,7 +128,7 @@ export function Modal({ open, setOpen, type }: ModalProps) {
             <Dialog.Close asChild>
               <button
                 className="Button green"
-                onClick={handleVerifyTransaction}
+                onClick={handleSubmit(handleVerifyTransaction)}
               >
                 Salvar
               </button>
